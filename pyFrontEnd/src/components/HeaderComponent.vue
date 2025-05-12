1cde0acc4620c35245fb64c01112aa3748db7aa9
KDABLM <kdablm@88.com>
2025-05-12 21:54:02 +0800

    修改搜索框

--- pyFrontEnd/src/components/HeaderComponent.vue
+++ pyFrontEnd/src/components/HeaderComponent.vue
@@ -3,9 +3,9 @@
 <template>
     <div class="h-15 sticky top-0 z-10 bg-white shadow-sm flex flex-row justify-between items-center px-2">
         <!-- 搜索框 -->
-        <div class="flex items-center justify-start w-1/6">
+        <div class="flex items-center justify-start w-1/10">
             <input type="text" name="price" id="price" class="input" placeholder="search" />
-            <button type="button" class="bg-blue-500 text-md outline-1 text-white rounded-md px-4 py-1 rounded-l-none">搜索</button>
+            <button type="button" class="bg-blue-500 text-sm cursor-pointer hover:bg-blue-600 w-1/6 min-w-15 h-7.5 outline-1 text-white rounded-md px-4 py-1 rounded-l-none">搜索</button>
         </div>
     </div>
 </template>
@@ -13,6 +13,6 @@
 @import "tailwindcss";
 
 .input {
-    @apply block rounded-l-md min-w-0 grow py-1 outline-1 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-blue-200 sm:text-sm/6 outline-blue-100
+    @apply block rounded-l-md grow py-1 h-7.5 outline-1 pr-3 pl-1 text-base text-gray-900 min-w-25 placeholder:text-gray-400 focus:outline-blue-200 sm:text-sm/6 outline-blue-100
 }
 </style>
